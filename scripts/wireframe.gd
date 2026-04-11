22b75999fd2a59edafb14c41a49a2bd79a38da19
Derin Aşır <derinasir@gmail.com>
2026-04-11 17:31:34 +0300

    feat: add dot fade out

--- scripts/wireframe.gd
+++ scripts/wireframe.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var dots_changed: bool = false
 var _sonar_dots: Array[SonarDot] = []
 
 
@@ -8,33 +7,48 @@ func _ready() -> void:
 	GameEvents.request_draw_dot.connect(_on_request_draw_dots)
 
 
-func _draw():
-	if dots_changed:
-		for dot: SonarDot in _sonar_dots:
-			draw_circle(to_local(dot.global_position), 5.0, dot.color)
-	dots_changed = false
+func _process(delta: float) -> void:
+	if _sonar_dots.is_empty():
+		return
+
+	var i = _sonar_dots.size() - 1
+	while i >= 0:
+		var dot = _sonar_dots[i]
+		dot.lifetime -= delta
+
+		if dot.lifetime <= 0:
+			_sonar_dots.remove_at(i)
+		else:
+			dot.color.a = dot.lifetime / dot.max_lifetime
+
+		i -= 1
+
+	queue_redraw()
+
+
+func _draw() -> void:
+	for dot: SonarDot in _sonar_dots:
+		draw_circle(to_local(dot.global_position), 5.0, dot.color)
 
 
 func match_group_color(group_name: String) -> Color:
 	var color: Color
 	match group_name:
 		"fuel_pack":
-			color = Color.YELLOW
+			color = Color(4.416, 4.416, 0.0)
 		"enemy":
-			color = Color.DARK_RED
+			color = Color(1.808, 0.0, 0.0)
 		"wall":
-			color = Color.BROWN
+			color = Color(4.416, 4.416, 4.416)
 		"void":
 			color = Color(Color.BLACK, 0.0)
 		_:
 			color = Color.WHITE
-
 	return color
 
 
 func _on_request_draw_dots(sonar_dots: Array[SonarDot]):
 	for dot in sonar_dots:
 		dot.color = match_group_color(dot.group)
-	_sonar_dots = sonar_dots.duplicate()
-	dots_changed = true
+		_sonar_dots.append(dot)
 	queue_redraw()
--- src/data/sonar_dot.gd
+++ src/data/sonar_dot.gd
@@ -4,8 +4,12 @@ extends RefCounted
 var global_position: Vector2 = Vector2.ZERO
 var color: Color
 var group: String
+var lifetime: float = 1.0
+var max_lifetime: float = 1.0
 
 
-func _init(p_global_pos: Vector2, p_group: String) -> void:
+func _init(p_global_pos: Vector2, p_group: String, p_lifetime: float = 1.0) -> void:
 	global_position = p_global_pos
 	group = p_group
+	lifetime = p_lifetime
+	max_lifetime = p_lifetime
