ec84fc11b1dc0bd8a8eb1163091a88a893165154
Tuna Eskan <tunaeskanaytac@gmail.com>
2026-04-12 03:32:41 +0300

    MainMenuDonei

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -1,12 +1,33 @@
 extends Control
 
-@export var initial_scene: StringName = &""
-@export var play_button: Button
+const GAME_SCENE: PackedScene = preload("uid://r66wywihg2l2")
 
-
+@onready var TutorialScreen: TextureRect = $TutorialScreen
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play_button.pressed.connect(_on_play_pressed)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_tutorial_pressed() -> void:
+	TutorialScreen.visible = true
+	pass # Replace with function body.
+
+
+func _on_tutorial_exit_button_pressed() -> void:
+	TutorialScreen.visible = false
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
 
 
 func _on_play_pressed() -> void:
-	SceneLoader.load_scene(initial_scene)
+	get_tree().change_scene_to_packed(GAME_SCENE)
+	pass # Replace with function body.
